d08cb81b03e810af7263ecf3893d6624224fb304
hhiep <hoaihiep12b1thptlochung2020@gmail.com>
2023-11-05 16:53:48 +0700

    hoan thanh trang DangKy

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import DangNhap from './components/layouts/DangNhap';
-
+import DangKy from './components/layouts/DangKy';
 
 const Stack = createNativeStackNavigator();
 
@@ -12,8 +12,9 @@ export default function App() {
   return (
     <View style={styles.container}>
       <NavigationContainer>
-        <Stack.Navigator>
+        <Stack.Navigator initialRouteName='DangKy'>
           <Stack.Screen name="DangNhap" component={DangNhap} options={{ headerShown: false }} />
+          <Stack.Screen name="DangKy" component={DangKy} options={{ headerShown: false }} />
         </Stack.Navigator>
       </NavigationContainer>
     </View>
