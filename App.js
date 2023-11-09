f70a5ec11f12d4dba545d05686bd5932e9b84ce6
hhiep <hoaihiep12b1thptlochung2020@gmail.com>
2023-11-09 14:42:31 +0700

    hoan thanh trang CaiDat

--- App.js
+++ App.js
@@ -6,6 +6,7 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import DangNhap from './components/layouts/DangNhap';
 import DangKy from './components/layouts/DangKy';
 import CaiDat from './components/layouts/CaiDat';
+import HocPhan from './components/layouts/HocPhan';
 
 
 
@@ -20,6 +21,7 @@ export default function App() {
           <Stack.Screen name="DangNhap" component={DangNhap} options={{ headerShown: false }} />
           <Stack.Screen name="DangKy" component={DangKy} options={{ headerShown: false }} />
           <Stack.Screen name="CaiDat" component={CaiDat} options={{ headerShown: false }} />
+          <Stack.Screen name="HocPhan" component={HocPhan} options={{ headerShown: false }} />
         </Stack.Navigator>
       </NavigationContainer>
     </View>
