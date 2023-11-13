261a8e3420300fba3f5373e39f63ab5d9a56c667
Your Name <you@example.com>
2023-11-13 21:08:26 +0700

    create screen ThuVien

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,Image } from 'react-native';
 
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
@@ -7,7 +7,8 @@ import DangNhap from './components/layouts/DangNhap';
 import DangKy from './components/layouts/DangKy';
 import CaiDat from './components/layouts/CaiDat';
 import HocPhan from './components/layouts/HocPhan';
-import TrangChu from './components/layouts/TrangChu';
+// import TrangChu from './components/layouts/TrangChu';
+import bottomTab from './components/layouts/bottomTab';
 
 
 
@@ -18,12 +19,14 @@ export default function App() {
   return (
     <View style={styles.container}>
       <NavigationContainer>
-        <Stack.Navigator initialRouteName='TrangChu'>
+        <Stack.Navigator initialRouteName='bottomTab'>
           <Stack.Screen name="DangNhap" component={DangNhap} options={{ headerShown: false }} />
           <Stack.Screen name="DangKy" component={DangKy} options={{ headerShown: false }} />
           <Stack.Screen name="CaiDat" component={CaiDat} options={{ headerShown: false }} />
           <Stack.Screen name="HocPhan" component={HocPhan} options={{ headerShown: false }} />
-          <Stack.Screen name="TrangChu" component={TrangChu} options={{ headerShown: false }} />
+          {/* <Stack.Screen name="TrangChu" component={TrangChu} options={{ headerShown: false }} /> */}
+          <Stack.Screen name="bottomTab" component={bottomTab} options={{ headerShown: false }} />
+        
         </Stack.Navigator>
       </NavigationContainer>
     </View>
---
+++ components/layouts/bottomTab.js
@@ -0,0 +1,88 @@
+import { StyleSheet, Text, View, Image } from 'react-native';
+
+import ThuVien from './ThuVien';
+import TrangChu from './TrangChu';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+export default function App({ navigation, route }) {
+
+    const BottomTab = createBottomTabNavigator();
+    const screens = ['TrangChu', "LoiGiai", "add", "ThuVien", "HoSO"]
+    const start = route.params?.start || 0
+    const sizeIcon = 40
+    return (
+        <BottomTab.Navigator
+            initialRouteName={screens[start]}
+            screenOptions={{
+                tabBarStyle: { backgroundColor: "#0A082D",borderTopWidth:3,height:65 },
+                tabBarLabelStyle:{fontSize:14},
+                tabBarActiveTintColor: "white",
+                tabBarInactiveTintColor: '#555E7A',
+                tabBarIconStyle:{size:15}
+            }}
+        >
+            <BottomTab.Screen
+                name={screens[0]}
+                component={TrangChu}
+                options={{
+                    headerShown: false,
+                    title: 'Trang chủ',
+                    tabBarIcon: ({ color, size }) => (
+                        <View><Image style={{ height: sizeIcon, width: sizeIcon, resizeMode: 'contain', tintColor: color }} source={require('../imgs/search.png')} /></View>
+                    )
+                }} />
+
+            <BottomTab.Screen
+                name={screens[1]}
+                component={TrangChu}
+                options={{
+                    headerShown: false,
+                    title: 'Lời giải',
+                    tabBarIcon: ({ color, size }) => (
+                        <View style={{ alignItems: 'center', justifyContent: 'center'}}><Image style={{ height: sizeIcon, width: sizeIcon, resizeMode: 'contain', tintColor: color }} source={require('../imgs/book.png')} /></View>
+                    )
+                }} />
+
+            <BottomTab.Screen
+                name={screens[2]}
+                component={TrangChu}
+                options={{
+                    headerShown: false,
+                    title: '',
+                    
+                    tabBarIcon: ({ color, size }) => (
+                        <View style={{ alignItems: 'center', justifyContent: 'center'}}><Image style={{ height: sizeIcon+10, width: sizeIcon+10, resizeMode: 'contain', tintColor: color }} source={require('../imgs/add.png')} /></View>
+                    )
+                }} />
+
+            <BottomTab.Screen
+                name={screens[3]}
+                component={ThuVien}
+                options={{
+                    headerShown: false,
+                    title: 'Thư viện',
+                    tabBarIcon: ({ color, size }) => (
+                        <View><Image style={{ height: sizeIcon, width: sizeIcon, resizeMode: 'contain', tintColor: color }} source={require('../imgs/Folder.png')} /></View>
+                    )
+                }} />
+
+            <BottomTab.Screen
+                name={screens[4]}
+                component={TrangChu}
+                options={{
+                    headerShown: false,
+                    title: 'Hồ sơ',
+                    tabBarIcon: ({ color, size }) => (
+                        <View><Image style={{ height: sizeIcon, width: sizeIcon, resizeMode: 'contain', tintColor: color }} source={require('../imgs/face.png')} /></View>
+                    )
+                }} />
+        </BottomTab.Navigator>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+    },
+});
